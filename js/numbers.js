0bc4692e3753d0c2f5fc6bfdaa4642f572ab030c
AlexChehov <0650343005id@gmail.com>
2023-01-12 22:48:11 +0200

    12-01-2023

---
+++ js/numbers.js
@@ -0,0 +1,25 @@
+const a = 1;
+const b = 2;
+const c = 3;
+
+
+// 0 - false
+// 1 - true
+
+console.log(10 === 10);
+console.log((10000 + 10) * 20);
+
+let cnst = 1;
+cnst = 2 ;
+const float = 1.5;
+
+console.log(float.toString(),"result1");
+console.log(float.toFixed(),"result2");
+
+
+const randomnumber =Math.floor( Math.random() * 10);
+console.log("random namber", randomnumber);
+
+
+console.log(cnst, "cnst");
+
---
+++ js/string.js
@@ -0,0 +1,48 @@
+// const name1 = "Igor ";
+// const surename = "Sergienko";
+
+// const fullname = name1.concat(surename, "ABC");
+// const firstLetter = name1[0];
+// console.log(firstLetter);
+
+// console.log(surename.includes("Igor"));
+// console.log(surename.toUpperCase());
+// console.log(surename.includes());
+
+// console.log(name1.trim());
+
+// console.log(surename.replace("o"), ("n"), "!!!")
+
+// alert(`Hello, ${name1} ${surename}`);
+
+
+
+// const sentance = "JavaScript, TypeScript, CoffeScriptr";
+// console.log(sentance.toLocaleLowerCase());
+// console.log(sentance.replaceAll("Script","script"));
+
+
+// const sentance = "JavaScript, TypeScript, CoffeScriptr";
+// console.log(sentance.replaceAll("," , "  ...")  );
+
+
+const sentance = "JavaScript, TypeScript, CoffeScriptr";
+const a = "javaScript";
+const b = "typeScript";
+
+
+
+console.log(b[0].slice() + a.slice(1, a.length));
+
+console.log(a[0].slice() + b.slice(1, b.length));
+
+
+
+
+// a[0] = "G";
+// console.log(a,"A");
+
+// console.log(a.replaceAll("java" , "Java"));
+// console.log(b.replaceAll("type" , "Type"));
+
+// console.log(a[0].toUpperCase()+ a.slise(1, a.length), "result");???????????
