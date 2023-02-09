7335a9b6076bd10c49970718c135d859e558cc5a
AlexChehov <113626854+AlexChehov@users.noreply.github.com>
2023-02-09 20:25:54 +0200

    09-02-2023

---
+++ index.js
@@ -0,0 +1,49 @@
+// const arr1 = ["Poland","Ukraine","USA"];
+// arr1.forEach((e,i)=>{
+//     arr1[i] = e[0]
+// });
+// console.log(arr1);
+
+// const arr = [1,5,17,4,8,6,7,0];
+// const arr2 = ["bananna","animal","flower"];
+
+// console.log(arr.sort((e1,e2) => e1 - e2),arr);
+// console.log(arr2.sort((e1,e2) => e1.localeCompare(e2)));
+
+// console.log("bananna".localeCompare("animal"),"locail compair")
+
+// const arr3 = [
+//     { name: "Yarik", age: 93 },
+//     { name: "Igor", age: 73 },
+//     { name: "Oleg", age: 43 },
+//     { name: "Olena", age: 23 },
+// ];
+// console.log(arr3.sort((e1, e2) => e1.name.localeCompare(e2.name)));
+
+// function fn1 (
+   // arguments
+    // ) {
+        //  body
+        //return value
+    // }                         //functiondeclaration
+// const fn2 = function () [] ;
+
+// const helloFunction = function() {
+//     const a = 10;
+//     const b = 15;
+//     alert(`Hello ${ a+b }`);
+// };
+// helloFunction();
+
+// const sum = function(a,b) {
+//     alert(`Hello ${ a+b }`);
+// };
+// sum(10,15);
+
+// const sum = function(a,b) {
+//     alert(`Hello ${ a+b }`);
+// };
+// const num1 = 100;
+// const num2 = 1000;
+// sum(num1,num2);
+
