b21e665e3ca112ba3061569f47e8b2524c6a00e2
AlexChehov <113626854+AlexChehov@users.noreply.github.com>
2023-03-02 21:23:15 +0200

    02-03-2023

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
-window.onload = () => {
-    alert("Hello I am loaded");
-};
+// window.onload = () => {
+//     alert("Hello I am loaded");
+// };
 
-window.onblur = () => {
-  document.title = "Come back please";  
-};
+// window.onblur = () => {
+//   document.title = "Come back please";  
+// };
 
-window.onclose = () => {
-    alert("Hey");
-};
+// window.onclose = () => {
+//     alert("Hey");
+// };
 
-window.onfocus = () => {
-    document.title = "Document";
-};
+// window.onfocus = () => {
+//     document.title = "Document2";
+// };
+
+const nameInput = document.querySelector("#name");
+const ageInput = document.querySelector("#age");
+const createButton = document.querySelector("#create");
+const userSection = document.querySelector("#users-section");
+
+const users = [];
+
+const renderUsers = () => {
+    userSection.innerHTML = "";
+    const UsersContent = users.map((user) => `<div>
+    <p>${user.name}</p>
+    <span>${user.age}</span>
+    </div>`);
+    console.log(UsersContent);
+
+
+    UsersContent.forEach((userLayout) => {
+        userSection.innerHTML += userLayout;
+    });
+}
+
+createButton.onclick = () => {
+    const name = nameInput.value;
+    const age = +ageInput.value;
+
+    const user = { name: name, age: age };
+
+    users.push(user);
+
+    nameInput.value = "";
+    ageInput.value = "";
+
+    renderUsers();
+}
