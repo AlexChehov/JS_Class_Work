ffdf86bc0a17042467dabd225dd3aa2ca1b4b09a
AlexChehov <113626854+AlexChehov@users.noreply.github.com>
2023-03-09 19:08:15 +0200

    09-03-2023

--- index.js
+++ index.js
@@ -2,63 +2,116 @@ const nameInput = document.querySelector("#name");
 const ageInput = document.querySelector("#age");
 const cityInput = document.querySelector("#city");
 const createButton = document.querySelector("#create");
-const userSection = document.querySelector("#users-section");
+const usersSection = document.querySelector("#users-section");
 const searchInput = document.querySelector("#search");
-const searchButton = document.querySelector("#search_button");
+const sortingByNameCheckbox = document.querySelector("#sort-by-name");
 
 let users = [
-    { nmae: "Igor", city: "Kiev", age: 23 },
-    { nmae: "Alex", city: "Kiev", age: 24 },
-    { nmae: "Oleg", city: "Kiev", age: 25 },
+  { name: "Igor", city: "Kyiv", age: 20 },
+  { name: "Alex", city: "Kyiv", age: 50 },
+  { name: "Oleg", city: "Kyiv", age: 10 },
 ];
 
-const deleteUser = (indexofusers) => {
-    users = users.filter((el, i) => i !== indexofusers);
-    renderUsers(users);
+let changingUser = undefined;
+
+renderUsers(users);
+
+const deleteUser = (indexOfUser) => {
+  users = users.filter((el, i) => i !== indexOfUser);
+  renderUsers(users);
 };
 
-function renderUsers (usersToRender) {
-    userSection.innerHTML = "";
-    const UsersContent = users.map((user) => `<div class="usersCard">
-    <p>${user.name}</p>
-    <span>${user.age}</span>
-    <span>${user.city}</span>
-    <button class="delete-user-button">Delete</button>
-    </div>`);
-    console.log(UsersContent);
+const editUser = (indexOfUser) => {
+  changingUser = {data: users[indexOfUser], index: indexOfUser};
 
+  createButton.textContent = "Save changes";
 
-    UsersContent.forEach((userLayout) => {
-        userSection.innerHTML += userLayout;
-    });
-    const deleteButtons = [...document.querySelectorAll(".delete-user-button")];
-
-    deleteButtons.forEach((button, index) => {
-        button.onclick = () => deleteUser(index);
-    });
+  nameInput.value = changingUser.data.name;
+  ageInput.value = changingUser.data.age;
+  cityInput.value = changingUser.data.city;
 };
 
+const sorting = {
+    names: () => {
+       const usersCopy = [...users]; 
+       usersCopy.sort((user1, user2) => user1.name.localeCompare(user2.name));
+       renderUsers(usersCopy);
+    }
+};
 
+function renderUsers(usersToRender) {
+  usersSection.innerHTML = "";
 
+  const usersContent = usersToRender.map(
+    (user) => `<div class="user-card">
+        <p>${user.name}</p>
+        <p>${user.city}</p>
+        <span>${user.age}</span>
+        <button class="delete-user-button">Delete</button>
+        <button class="edit-user-button">Edit</button>
+    </div>`
+  );
+
+  usersContent.forEach((userLayout) => {
+    usersSection.innerHTML += userLayout;
+  });
+
+  const deleteButtons = [...document.querySelectorAll(".delete-user-button")];
+
+  deleteButtons.forEach((button, i) => {
+    button.onclick = () => deleteUser(i);
+  });
+
+  const editButtons = [...document.querySelectorAll(".edit-user-button")];
+
+  editButtons.forEach((button, i) => {
+    button.onclick = () => editUser(i);
+  });
+}
 
 createButton.onclick = () => {
-    const name = nameInput.value;
-    const age = +ageInput.value;
-    const city = cityInput.value;
+  const name = nameInput.value;
+  const age = +ageInput.value;
+  const city = cityInput.value;
 
+  if (changingUser) {
+   
+    users[changingUser.index] = {
+        name: name,
+        age: age,
+        city: city
+    };
+
+    changingUser = undefined;
+    createButton.textContent = "Create User";
+  } else {
     const user = { name: name, age: age, city: city };
 
     users.push(user);
+  }
 
-    nameInput.value = "";
-    ageInput.value = "";
-    cityInput.value = "";
+  nameInput.value = "";
+  ageInput.value = "";
+  cityInput.value = "";
 
-    renderUsers(users);
+  renderUsers(users);
 };
 
-searchButton.onclick = () => {
-    const usersToRender = users.filter((user) => user.name.includes(searchInput.value));
-    // console.log(usersToRender);
-    renderUsers(usersToRender);
+searchInput.oninput = (event) => {
+  const usersToRender = users.filter(({ name, age, city }) =>
+    [name, age.toString(), city].some((element) =>
+      element.includes(event.target.value)
+    )
+  );
+
+  renderUsers(usersToRender);
 };
+
+
+sortingByNameCheckbox.onchange = (event) => {
+    if (event.target.checked) {
+        sorting.names();
+    } else {
+        renderUsers(users);
+    }
+}
